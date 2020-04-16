597502d970d54bea7a1365fed087a0bfdabd7702
Deojeff Lai <d30jeff@gmail.com>
2020-04-16 19:36:47 +0800

    Tweak before install script

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 # This script is executed before copying the source
+
+yum -y update
+
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+
+source ~/.bashrc
+
+nvm install 12.12.0 && nvm alias default 12.12.0
+
+curl -s https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.js | node
+
+yvm install latest
+
 rm -rf /usr/code
 mkdir -p /usr/code
