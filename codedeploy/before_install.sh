ced86291f8f32d4e5f55e1d3aba4ea505797c299
Deojeff Lai <d30jeff@gmail.com>
2020-04-16 19:51:52 +0800

    Add nvm init

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -7,7 +7,9 @@ yum -y update
 # Install NVM
 curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
 
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 
 nvm install 12.12.0 && nvm alias default 12.12.0
 
