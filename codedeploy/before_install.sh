c14edfc086f4e8136d3153059323084763b2af50
Deojeff Lai <d30jeff@gmail.com>
2020-04-17 11:32:47 +0800

    Install node

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -5,7 +5,8 @@
 yum -y update
 yum install -y gcc-c++ make
 
-curl -sL https://rpm.nodesource.com/setup_12.x | -E bash -
+curl -sL https://rpm.nodesource.com/setup_12.x | bash -
+yum install nodejs
 
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 yum install yarn -y
