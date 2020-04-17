ab62964e6ba884738dbc7f32c62f38c5c2f5c6c6
Deojeff Lai <d30jeff@gmail.com>
2020-04-17 10:44:28 +0800

    Install yarn

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -12,6 +12,5 @@ export NVM_DIR="$HOME/.nvm"
 
 nvm install 12.12.0 && nvm alias default 12.12.0
 
-curl -s https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.js | node
-
-yvm install latest
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+yum install yarn -y
