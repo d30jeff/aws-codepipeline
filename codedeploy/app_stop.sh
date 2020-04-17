c814e4beb3042b6545c02d8d72859b3085ab574b
Deojeff Lai <d30jeff@gmail.com>
2020-04-17 20:13:49 +0800

    Update message

--- codedeploy/app_stop.sh
+++ codedeploy/app_stop.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # This script is executed after the source is copied to the instances
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 cd /home/ec2-user
 
