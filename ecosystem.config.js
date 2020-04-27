64a2f12c86b246b7c633333881380710b9da7907
Deojeff Lai <d30jeff@gmail.com>
2020-04-27 14:41:24 +0800

    Add cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'mobile-api',
+      cwd: '/home/ec2-user/mobile-api',
       script: 'dist/apps/mobile-api/main.js',
       instances: 1,
       autorestart: true,
